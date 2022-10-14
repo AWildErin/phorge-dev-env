0a5120b92b5b9bd8e26052b8f2f4a09090791793
AWildErin <iansharp1@live.co.uk>
2022-10-14 02:45:11 +0100

    feat: Container works
    add more options later

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 ## How to use this
 1. Clone the repo
 2. Do not clone the submodules (VERY IMPORTANT, this breaks if you do this through windows, so we will let the container handle that for us)
+3. Add `127.0.0.1:8080 phorge.local` to your hosts file
 3. Open the container
--- scripts/phorge_setup.sh
+++ scripts/phorge_setup.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 cd /workspace/phorge
-sudo ./bin/config set mysql.host '127.0.0.1'
-sudo ./bin/config set mysql.user 'root'
-sudo ./bin/config set mysql.pass 'mariadb'
+./bin/config set mysql.host '127.0.0.1'
+./bin/config set mysql.user 'root'
+./bin/config set mysql.pass 'mariadb'
+
+./bin/config set phabricator.base-uri http://phorge.local
+
+./bin/config set phabricator.developer-mode true
+./bin/config set phabricator.show-prototypes true
+./bin/config set darkconsole.enabled true
+
+./bin/storage upgrade --force
